5b801d54872f7f3540ae0724b71f8c15ca53f3de
Diego Ampudia <diego.ampudia@accelya.com>
2023-08-02 22:44:59 +0200

    base project

---
+++ src/main/java/net/fag/picowh/manager/PicowhManagerApplication.java
@@ -0,0 +1,13 @@
+package net.fag.picowh.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicowhManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicowhManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/fag/picowh/manager/PicowhManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.fag.picowh.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicowhManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
